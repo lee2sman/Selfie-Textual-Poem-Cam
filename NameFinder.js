33eaef8b93cf2a51b31733231bd349f6303b9539
Lee Tusman <leetusman@gmail.com>
2014-04-30 22:18:39 -0400

    Initial upload of Casper.js file

---
+++ NameFinder.js
@@ -0,0 +1,30 @@
+var selfieText;
+var casper = require('casper').create();
+
+function getoutputText() {
+    var outputText = document.querySelector('#ires');
+    return outputText.textContent;
+}
+
+var name = casper.cli.get(0);
+
+if (!name) {
+    casper.echo('please provide a word').exit(1);
+}
+
+casper.start('http://google.fr/', function() {
+    // search for entered name in google form
+    this.fill('form[action="/search"]', { q: name }, true);
+});
+
+casper.then(function() {
+    // aggregate results for the 'phantomjs' search
+    selfieText = this.evaluate(getoutputText);
+});
+
+casper.run(function() {
+    var myfile = "newfile.txt"
+    var fs = require('fs');
+    fs.write(myfile, selfieText, 'w');
+    this.exit();
+});
